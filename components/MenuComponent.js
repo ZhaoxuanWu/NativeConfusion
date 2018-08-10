6d778ca8ced5c7418bedfe3e767b45d903b04015
Wu Zhaoxuan <wu.zhaoxuan96@gmail.com>
2018-08-10 17:04:19 +0800

    Components Part 1

---
+++ components/MenuComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+function Menu(props) {
+
+  const renderMenuItem = ({item, index}) => {
+    return(
+      <ListItem
+        leftAvatar
+        key={index}
+        title={item.name}
+        subtitle={item.description}
+        hideChevron={true}
+        avatar={{ source: require('./images/elaicheesecake.png')}}
+      />
+    )
+  }
+
+  return(
+    <FlatList style={{marginTop: 50}}
+      data={props.dishes}
+      renderItem={renderMenuItem}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
+
+}
+
+export default Menu;
