62a93bce7c8b5735195d7283874630d95f071cb5
Wu Zhaoxuan <wu.zhaoxuan96@gmail.com>
2018-08-15 12:09:03 +0800

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -81,6 +82,25 @@ const AboutNavigator = createStackNavigator({
       />
   })
 })
+
+const ReservationNavigator = createStackNavigator({
+  Reservation: { screen: Reservation },
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: '#512DA8'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      color: '#fff'
+    },
+    headerLeft:
+      <Icon name='menu' size={24} color='white'
+        onPress={() => navigation.toggleDrawer()}
+      />
+  })
+})
+
 const ContactNavigator = createStackNavigator({
   Contact: { screen: Contact },
 }, {
@@ -158,6 +178,16 @@ const MainNavigator = createDrawerNavigator({
       drawerLabel: 'Contact Us',
       drawerIcon: ({ tintColor }) => (
         <Icon name='address-card' type='font-awesome' size={22} color={tintColor} />
+      )
+    }
+  },
+  Reservation: {
+    screen: ReservationNavigator,
+    navigationOptions: {
+      title: 'Reserve Table',
+      drawerLabel: 'Reserve Table',
+      drawerIcon: ({ tintColor }) => (
+        <Icon name='cutlery' type='font-awesome' size={24} color={tintColor} />
       )
     }
   }
